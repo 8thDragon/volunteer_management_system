34fce2c394544da432057c69ec9fba7040872256
Panthorn60 <s6001012630063@email.kmutnb.ac.th>
2023-04-18 23:37:22 +0700

    pp

--- src/activities/activities.controller.ts
+++ src/activities/activities.controller.ts
@@ -152,7 +152,7 @@ export class ActivitiesController {
 
   // @Patch('start_activity')
   // @Interval(10000)
-  @Cron('1,11,21,31,41,51 * * * * *')
+  @Cron('* 0,5,10,15,20,25,30,35,40,45,50,55 * * * *')
   async startActivity() {
     return this.activitiesService.startActivity()
   }
